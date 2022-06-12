3e54b8a36f9ccbdd2052ef735a60b700aefd8e08
od3ng <noprianto@polinema.ac.id>
2022-06-12 20:09:36 +0700

    add dataset

---
+++ src/main/java/jobsheet12/kuis2/DeptEmpStrengthMapper.java
@@ -0,0 +1,22 @@
+package jobsheet12.kuis2;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+
+public class DeptEmpStrengthMapper extends MapReduceBase implements Mapper<LongWritable, Text, TextPair, Text> {
+
+    @Override
+    public void map(LongWritable key, Text value, OutputCollector<TextPair, Text> output, Reporter reporter)
+            throws IOException {
+
+        String valueString = value.toString();
+        String[] SingleNodeData = valueString.split("\t");
+        output.collect(new TextPair(SingleNodeData[0], "1"), new Text(SingleNodeData[1]));
+    }
+}
